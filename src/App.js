c3ca2a679e99ed1f35faf4875d1c5c8fda39aac2
Gareth Bk <garethbk@gmail.com>
2018-04-11 23:15:50 -0700

    Ready for remote

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { Provider } from 'unstated';
+import PageTransition from 'react-router-page-transition';
 import logo from './logo.svg';
 import './App.css';
+//import './main.styl';
+
+import Landing from './components/Landing';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Provider>
+          <Router>
+            <Route
+              render={({ location }) => (
+                <PageTransition>
+                  <Switch location={location}>
+                    <Route exact path="/" component={Landing} />
+                    <Route path="/about" component={About} />
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/contact" component={Contact} />
+                  </Switch>
+                </PageTransition>
+              )}
+            />
+          </Router>
+        </Provider>
       </div>
     );
   }
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -5,14 +5,53 @@ class AppContainer extends Container {
     super();
 
     this.state = {
-      title: "I'm Gareth.  I like to build stuff.",
-      tagline: 'I get excited about cooking, running, cycling, reading, and JavaScript. My favorite CSS color is ',
       email: 'garethbk@gmail.com',
       phone: '540 454 7597',
-      loadedRoute: '/'
+      loadedRoute: 'Home',
+      about: { name: 'Gareth Bk', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/misc/gbk-chicken.jpg' },
+      skills: [
+        { skill: 'HTML5', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/html5-logo.png' },
+        { skill: 'CSS3', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/css3-logo.png' },
+        { skill: 'JavaScript', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/js-logo.png' },
+        { skill: 'Sass', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/sass-logo.png' },
+        { skill: 'Stylus', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/stylus-logo.png' },
+        { skill: 'Node', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/nodejs-logo.png' },
+        { skill: 'React', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/react-logo.png' },
+        { skill: 'Redux', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/redux-logo.png' },
+        { skill: 'Unstated', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/unstated-logo.png' },
+        { skill: 'WordPress', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/wordpress-logo.png' },
+        { skill: 'PHP', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/php-logo.png' },
+        { skill: 'Phaser', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/phaser-logo.png' },
+        { skill: 'MongoDB', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/mongodb-logo.png' },
+        { skill: 'MySQL', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/mysql-logo.png' },
+        { skill: 'Photoshop', img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/logos/photoshop-logo.png' }
+      ],
+      projects: [
+        {
+          name: 'Origin Portal',
+          desc: 'Hired.com for bootcamp grads.',
+          img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/projects/origin-portal/origin-portal-landing.jpg'
+        },
+        {
+          name: 'Stravisualizer',
+          desc: 'Strava data visualizer.',
+          img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/projects/stravisualize/stravisualize.gif'
+        },
+        {
+          name: '9 Lives',
+          desc: 'A game about a cat.',
+          img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/projects/nine-lives/nine-lives.jpg'
+        },
+        {
+          name: 'Hardywood Park Craft Brewery',
+          desc: 'WordPress brewery website.',
+          img: 'https://s3.us-east-2.amazonaws.com/garethbk-portfolio/projects/hardywood/hardywood.jpg'
+        }
+      ]
     };
   }
   setRoute = input => {
+    input = input.charAt(1).toUpperCase() + input.slice(2);
     this.setState({
       loadedRoute: input
     });
