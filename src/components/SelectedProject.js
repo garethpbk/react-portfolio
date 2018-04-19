ae70bcf23a2934ae59472e52f6d40f15b0a9010b
Gareth Bk <garethbk@gmail.com>
2018-04-19 16:03:49 -0700

    Added back button to projects

--- src/components/SelectedProject.js
+++ src/components/SelectedProject.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Subscribe } from 'unstated';
 import AppContainer from '../containers/AppContainer';
 
-import { HeightContainer, FlexContainer, ContentContainer, RouteButton } from './StyleBase';
+import { HeightContainer, FlexContainer, ContentContainer, RouteButton, DisplayInlineBlock } from './StyleBase';
 
-const SelectedProject = props => {
-  return (
-    <Subscribe to={[AppContainer]}>
-      {appState => (
-        <HeightContainer
-          className="transition-item transitioned-component-right"
-          bgColor={'rgba(' + appState.state.loadedProject.bgColor + ', 0.75)'}
-        >
-          <FlexContainer>
-            <ContentContainer>
-              <img src={appState.state.loadedProject.img} alt={appState.state.loadedProject.name} />
-              <h1>{appState.state.loadedProject.name}</h1>
-              <p>{appState.state.loadedProject.desc}</p>
-            </ContentContainer>
-          </FlexContainer>
-        </HeightContainer>
-      )}
-    </Subscribe>
-  );
-};
+export default class SelectedProject extends Component {
+  constructor() {
+    super();
 
-export default SelectedProject;
+    this.goToRoute = this.goToRoute.bind(this);
+  }
+  goToRoute(e) {
+    this.props.history.push({ pathname: e });
+  }
+  render() {
+    return (
+      <Subscribe to={[AppContainer]}>
+        {appState => (
+          <HeightContainer
+            className="transition-item transitioned-component-right"
+            bgColor={'rgba(' + appState.state.loadedProject.bgColor + ', 0.75)'}
+          >
+            <FlexContainer>
+              <ContentContainer>
+                <img src={appState.state.loadedProject.img} alt={appState.state.loadedProject.name} />
+                <h1>{appState.state.loadedProject.name}</h1>
+                <p>{appState.state.loadedProject.desc}</p>
+
+                <RouteButton
+                  value="/projects"
+                  onClick={event => {
+                    this.goToRoute(event.target.value);
+                    appState.setRoute(event.target.value);
+                  }}
+                  style={DisplayInlineBlock}
+                >
+                  Go Back <span className="rotate-left">▲</span>
+                </RouteButton>
+              </ContentContainer>
+            </FlexContainer>
+          </HeightContainer>
+        )}
+      </Subscribe>
+    );
+  }
+}
